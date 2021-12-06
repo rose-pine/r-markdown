e63b8db022f40ca089826011883244158db3854d
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-12-06 13:07:07 -0800

    Create ggthemr_rose_pine.R

---
+++ ggthemr_rose_pine.R
@@ -0,0 +1,12 @@
+rose_pine <- define_palette(
+  background = '#1f1d2e', 
+  text = c('#555169', '#e0def4'), 
+  line = c('#e0def4', '#c4a7e7'),
+  gridline = '#504940',
+  swatch = c(
+    '#e0def4',
+    '#eb6f92', '#f6c177', 
+    '#ebbcba', '#31748f', 
+    '#9ccfd8', '#c4a7e7'),
+  gradient = c('#31748f', '#9ccfd8')
+)
